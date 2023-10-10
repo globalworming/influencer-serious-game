bb0a90535fbe7643e6184295ac3a5f4b81fabd9b
globalworming <andreas.worm@headissue.com>
2023-10-10 15:51:09 +0200

    release v0.0.1

---
+++ release.sh
@@ -0,0 +1,14 @@
+set -e
+npm version --no-git-tag-version $1
+git add package.json
+git add package-lock.json
+
+rm -rf docs
+npm run build
+mv build docs
+git add -a docs
+
+git commit -m "release v$1"
+git tag $1
+git push && git push --tags
+
--- sh release.sh
+++
@@ -1,4 +0,0 @@
-rm -rf docs
-npm run build
-mv build docs
-git add -a docs
