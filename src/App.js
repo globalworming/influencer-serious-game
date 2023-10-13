0a338f526e9be817fe6c4366742aa02f3a69f1d2
globalworming <andreas.worm@headissue.com>
2023-10-13 13:05:45 +0200

    end screen, events

--- src/App.js
+++ src/App.js
@@ -1,44 +1,37 @@
 import {useContext, useState} from "react"
 import EventComponent from "./component/Event";
-import {AppBar, Box, Button, Toolbar, Typography} from "@mui/material";
+import {Box, Button, Typography} from "@mui/material";
 import {StatsContext} from "./context/StatsContext";
 import Events from "./model/Events";
-import Stat from "./component/Stat";
+import Header from "./component/Header";
 
 const App = () => {
     const {
-        notorietyState: [noto, setNoto],
-        popularityState: [pop, setPop],
-        moneyState: [money, setMoney],
-        privateLifeState: [life, setLife],
+        notorietyState: [, setNoto],
+        popularityState: [, setPop],
+        moneyState: [, setMoney],
+        privateLifeState: [, setLife],
     } = useContext(StatsContext);
     const [index, setIndex] = useState(0)
     const end = index >= Events.length;
 
+
     return (
         <>
-            <AppBar position="static">
-                <Toolbar sx={{justifyContent: "space-around", columnGap: 4, flexWrap: "wrap", maxWidth: 700, margin: "0 auto"}}>
-                    <Stat label={"Bekanntheit"} value={noto}/>
-                    <Stat label={"Beliebtheit"} value={pop}/>
-                    <Stat label={"Geld"} value={money}/>
-                    <Stat label={"Privatleben"} value={life}/>
-                </Toolbar>
-            </AppBar>
-
+            <Header end={end}/>
             <Box sx={{p: 1, width: 1.0, display: "flex", justifyContent: "center"}}>
                 {!end && <EventComponent event={Events[index]} next={() => setIndex(index + 1)}/>}
                 {end && <>
                     <Box
                         sx={{width: 1.0, display: "flex", alignItems: "center", flexDirection: "column", gap: 4, p: 4}}>
-                        <Typography variant={"h3"}>you did it!</Typography>
+                        <Typography variant={"h3"}>Nochmal?</Typography>
                         <Typography><Button variant={"outlined"} onClick={() => {
                             setIndex(0)
                             setNoto(0)
                             setPop(0)
                             setMoney(0)
                             setLife(0)
-                        }}>again!</Button></Typography>
+                        }}>Neu beginnen.</Button></Typography>
                     </Box>
                 </>}
             </Box>
